e25bad38cddcbfc1aed3c9aa6651661933f0d176
BartlomiejLeja <bartlomiejleja@gmail.com>
2017-11-27 17:36:33 +0100

    Almost done Hebb rule

--- PerceptronLerning/HebbRule/HebbRule.cs
+++ PerceptronLerning/HebbRule/HebbRule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PerceptronLerning.HebbRule
 {
@@ -7,22 +8,33 @@ namespace PerceptronLerning.HebbRule
         private double[] _weight { get; set; }
         private double a;
         private double s;
-        private double c = 1;
+        private double c = 0.1;
+        private double [] cap;
+        private double gamma = 0.1;
 
         void adjustWeight(double [] input)
         {
             for(int i=0;i<input.Length;i++)
             {
-                s += input[i] * _weight[i];
+                s +=  _weight[i]* input[i];
             }
-            a = (a > 0) ? 1 : 0;
+            a = (s > 0) ? 1 : -1;
+            cap = new double[_weight.Length];
             for(int i=0;i< _weight.Length;i++)
             {
-                _weight[i] = _weight[i] + (c * a) * input[i];
+                cap[i] = (c * a) * input[i];
             }
+            for (int i = 0; i < _weight.Length; i++)
+            {
+                _weight[i] = _weight[i]/**(1-gamma)*/ + cap[i];
+             //   Console.Write($"{Math.Round(_weight[i], 2)} ");
+            }
+            s = 0;
+            a = 0;
+           // Console.WriteLine();
         }
 
-       public void adjustWeights(double [][] input, int timeOfLerning)
+        public void adjustWeights(double [][] input, int timeOfLerning)
         {
             drawWeight();
             string textForOutput = String.Empty;
@@ -31,13 +43,10 @@ namespace PerceptronLerning.HebbRule
                 for(int j =0;j<20;j++)
                 {
                     adjustWeight(input[j]);
+                
                 }
-                foreach (var w in _weight)
-                {
-                    textForOutput += $"{(int)w} ";
-                }
-                Console.WriteLine(textForOutput);
-                textForOutput = String.Empty;
+                //Console.WriteLine(textForOutput);
+                //textForOutput = String.Empty;
             }
 
         }
@@ -47,9 +56,10 @@ namespace PerceptronLerning.HebbRule
             double testResult=0;
             for (int i = 0; i < testInput.Length; i++)
             {
-                 testResult += testInput[i] * _weight[i]; 
+                 testResult +=  (testInput[i] * _weight[i]);
             }
-            testResult = (testResult > 0) ? 1 : 0;
+            Console.WriteLine($"{testResult}");
+            testResult = (testResult > 0) ? 1 : -1;
             Console.WriteLine($"{testResult}");
         }
 
@@ -58,7 +68,7 @@ namespace PerceptronLerning.HebbRule
             _weight = new double[35];
             var randomNumber = new Random();
             for (int i = 0; i < 35; i++)
-                _weight[i] = -10 + randomNumber.NextDouble() * 20;
+                _weight[i] = randomNumber.NextDouble() ;
         }
     }
 }
--- PerceptronLerning/Program.cs
+++ PerceptronLerning/Program.cs
@@ -7,10 +7,10 @@ namespace PerceptronLerning
     {
         static void Main(string[] args)
         {
-            var dictionaryInput = new LetterDataTextFileReader();
+           // var dictionaryInput = new LetterDataTextFileReader();
             // //var letterInput = dictionaryInput.ConvertTextForArray(@"C:\Users\BartlomiejLeja\source\repos\PerceptronLerning\PerceptronLerning\letterLearningData.txt");
 
-            var floatLetterInput = dictionaryInput.ConvertTextForFloatArray(@"C:\Users\BartlomiejLeja\source\repos\PerceptronLerning\PerceptronLerning\letterLearningData.txt");
+           // var floatLetterInput = dictionaryInput.ConvertTextForFloatArray(@"C:\Users\BartlomiejLeja\source\repos\PerceptronLerning\PerceptronLerning\letterLearningData.txt");
             // //var test = new LogicalDataTextFileReader();
             // //var input =test.ConvertTextForArray(@"C:\Users\BartlomiejLeja\source\repos\PerceptronLerning\PerceptronLerning\testData.txt");
             // //var test1 = new Neuron(input,15,0.1);
@@ -18,27 +18,27 @@ namespace PerceptronLerning
 
             // //var oneLayerNetworkTest = new OneLayerNetwork(letterInput,0.5);
 
-            var net = new BackpropagationNeuralNetwork(new int[] { 35, 35, 35, 20 }); //intiilize network 3 input (0 0 0) 2 hidden layers with 25 neurons 1 output (1)
+           // var net = new BackpropagationNeuralNetwork(new int[] { 35, 35, 35, 20 }); //intiilize network 3 input (0 0 0) 2 hidden layers with 25 neurons 1 output (1)
 
             //Itterate 5000 times and train each possible output
             //5000*8 = 40000 traning operations
-            for (int i = 0; i < 5000; i++)
-            {
-                for (int j = 0; j < floatLetterInput.Count; j++)
-                {
-                    net.FeedForward(floatLetterInput[j].LetterPattern);
-                    net.BackProp(floatLetterInput[j].LetterResult);
-                }
+            //for (int i = 0; i < 5000; i++)
+            //{
+            //    for (int j = 0; j < floatLetterInput.Count; j++)
+            //    {
+            //        net.FeedForward(floatLetterInput[j].LetterPattern);
+            //        net.BackProp(floatLetterInput[j].LetterResult);
+            //    }
 
-            }
-
-            
-            net.TestMethod(new float[] { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1 }, 'A');
-            net.TestMethod(new float[] { 1, 1, 1, 1, 0, 1, 0 ,0, 0, 1, 1, 0, 0 ,0 ,1, 1 ,1 ,1 ,1 ,0, 1 ,0, 0 ,0 ,1 ,1 ,0 ,0, 0, 1, 1, 1, 1, 1, 0 }, 'B');
+            //}
 
 
-            //var test = new HebbRuleHandler();
-            //test.HebbRuleShower();
+            //net.TestMethod(new float[] { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1 }, 'A');
+            //net.TestMethod(new float[] { 1, 1, 1, 1, 0, 1, 0 ,0, 0, 1, 1, 0, 0 ,0 ,1, 1 ,1 ,1 ,1 ,0, 1 ,0, 0 ,0 ,1 ,1 ,0 ,0, 0, 1, 1, 1, 1, 1, 0 }, 'B');
+
+
+            var test = new HebbRuleHandler();
+            test.HebbRuleShower();
 
             Console.ReadKey();
         }
